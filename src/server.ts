ab8390a464f8e30aefd811f298e50368bbbbb445
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-02-10 18:34:54 +0900

    adicionado ordenação, genero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import express from "express"
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
-const app = express()
+const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async(req, res)=>{
-    const movies = await prisma.movie.findMany();
-    res.json(movies)
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: { title: "asc" },
+    include: { genres: true, languages: true },
+  });
+  res.json(movies);
 });
 
-app.listen(port, ()=>{
-    console.log(`servidor executando no http://localhost:${port}`);
-    
+app.listen(port, () => {
+  console.log(`servidor executando no http://localhost:${port}`);
 });
