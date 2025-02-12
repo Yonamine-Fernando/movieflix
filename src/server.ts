53e7b7a8589cc04ad7041ac1258b4ee5f1670fa9
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-02-13 05:51:08 +0900

    adicionando correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,20 @@ app.post("/movies", async (req, res) => {
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
   try {
+    const movieWithSameTitle = await prisma.movie.findFirst({
+      where: {
+        title: { equals: title, mode: "insensitive" },
+      },
+    });
+
+    if (movieWithSameTitle) {
+      return res
+        .status(409)
+        .send({
+          message: "Já existe um filme com esse título/ filme cadastrado",
+        });
+    }
+
     await prisma.movie.create({
       data: {
         title,
@@ -29,7 +43,7 @@ app.post("/movies", async (req, res) => {
       },
     });
   } catch (error) {
-    return res.status(500).send({message:'fala ao cadastrar filme'})
+    return res.status(500).send({ message: "fala ao cadastrar filme" });
   }
   res.status(201).send();
 });
